ce0e8e7825e116324c702d521eac3e8467d55b84
kshvakov <shvakov@gmail.com>
2017-01-18 17:53:35 +0200

    add temporary table test

--- clickhouse_test.go
+++ clickhouse_test.go
@@ -221,3 +221,32 @@ func Test_Tx(t *testing.T) {
 		}
 	}
 }
+
+func Test_Temporary_Table(t *testing.T) {
+	const (
+		ddl = `
+			CREATE TEMPORARY TABLE clickhouse_test_temporary_table (
+				ID UInt64
+			);
+		`
+	)
+	if connect, err := sql.Open("clickhouse", "tcp://127.0.0.1:9000?debug=true"); assert.NoError(t, err) {
+		if tx, err := connect.Begin(); assert.NoError(t, err) {
+			if _, err := tx.Exec(ddl); assert.NoError(t, err) {
+				if _, err := tx.Exec("INSERT INTO clickhouse_test_temporary_table (ID) SELECT number AS ID FROM system.numbers LIMIT 10"); assert.NoError(t, err) {
+					if rows, err := tx.Query("SELECT ID FROM clickhouse_test_temporary_table"); assert.NoError(t, err) {
+						var count int
+						for rows.Next() {
+							var num int
+							if err := rows.Scan(&num); !assert.NoError(t, err) {
+								return
+							}
+							count++
+						}
+						assert.Equal(t, int(10), count)
+					}
+				}
+			}
+		}
+	}
+}
