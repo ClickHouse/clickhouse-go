ceba07f7bb8e60218a2f4110a90f044d657b815c
Alex Bocharov <ab@cloudflare.com>
2018-07-11 11:47:11 +0100

    Add Nullable support for writing data in blocks.

--- lib/binary/encoder.go
+++ lib/binary/encoder.go
@@ -19,12 +19,26 @@ type Encoder struct {
 	scratch [binary.MaxVarintLen64]byte
 }
 
+func (enc *Encoder) Nullable() error {
+	if _, err := enc.output.Write([]byte{0}); err != nil {
+		return err
+	}
+	return nil
+}
+
 func (enc *Encoder) Uvarint(v uint64) error {
 	ln := binary.PutUvarint(enc.scratch[:binary.MaxVarintLen64], v)
 	if _, err := enc.output.Write(enc.scratch[0:ln]); err != nil {
 		return err
 	}
 	return nil
+}
+
+func (enc *Encoder) UvarintNullable(v uint64) error {
+	if err := enc.Nullable(); err != nil {
+		return err
+	}
+	return enc.Uvarint(v)
 }
 
 func (enc *Encoder) Bool(v bool) error {
@@ -34,20 +48,55 @@ func (enc *Encoder) Bool(v bool) error {
 	return enc.UInt8(0)
 }
 
+func (enc *Encoder) BoolNullable(v bool) error {
+	if err := enc.Nullable(); err != nil {
+		return err
+	}
+	return enc.Bool(v)
+}
+
 func (enc *Encoder) Int8(v int8) error {
 	return enc.UInt8(uint8(v))
+}
+
+func (enc *Encoder) Int8Nullable(v int8) error {
+	if err := enc.Nullable(); err != nil {
+		return err
+	}
+	return enc.Int8(v)
 }
 
 func (enc *Encoder) Int16(v int16) error {
 	return enc.UInt16(uint16(v))
 }
 
+func (enc *Encoder) Int16Nullable(v int16) error {
+	if err := enc.Nullable(); err != nil {
+		return err
+	}
+	return enc.Int16(v)
+}
+
 func (enc *Encoder) Int32(v int32) error {
 	return enc.UInt32(uint32(v))
 }
 
+func (enc *Encoder) Int32Nullable(v int32) error {
+	if err := enc.Nullable(); err != nil {
+		return err
+	}
+	return enc.Int32(v)
+}
+
 func (enc *Encoder) Int64(v int64) error {
 	return enc.UInt64(uint64(v))
+}
+
+func (enc *Encoder) Int64Nullable(v int64) error {
+	if err := enc.Nullable(); err != nil {
+		return err
+	}
+	return enc.Int64(v)
 }
 
 func (enc *Encoder) UInt8(v uint8) error {
@@ -56,6 +105,13 @@ func (enc *Encoder) UInt8(v uint8) error {
 		return err
 	}
 	return nil
+}
+
+func (enc *Encoder) UInt8Nullable(v uint8) error {
+	if err := enc.Nullable(); err != nil {
+		return err
+	}
+	return enc.UInt8(v)
 }
 
 func (enc *Encoder) UInt16(v uint16) error {
@@ -67,6 +123,13 @@ func (enc *Encoder) UInt16(v uint16) error {
 	return nil
 }
 
+func (enc *Encoder) UInt16Nullable(v uint16) error {
+	if err := enc.Nullable(); err != nil {
+		return err
+	}
+	return enc.UInt16(v)
+}
+
 func (enc *Encoder) UInt32(v uint32) error {
 	enc.scratch[0] = byte(v)
 	enc.scratch[1] = byte(v >> 8)
@@ -76,6 +139,13 @@ func (enc *Encoder) UInt32(v uint32) error {
 		return err
 	}
 	return nil
+}
+
+func (enc *Encoder) UInt32Nullable(v uint32) error {
+	if err := enc.Nullable(); err != nil {
+		return err
+	}
+	return enc.UInt32(v)
 }
 
 func (enc *Encoder) UInt64(v uint64) error {
@@ -93,12 +163,33 @@ func (enc *Encoder) UInt64(v uint64) error {
 	return nil
 }
 
+func (enc *Encoder) UInt64Nullable(v uint64) error {
+	if err := enc.Nullable(); err != nil {
+		return err
+	}
+	return enc.UInt64(v)
+}
+
 func (enc *Encoder) Float32(v float32) error {
 	return enc.UInt32(math.Float32bits(v))
 }
 
+func (enc *Encoder) Float32Nullable(v float32) error {
+	if err := enc.Nullable(); err != nil {
+		return err
+	}
+	return enc.Float32(v)
+}
+
 func (enc *Encoder) Float64(v float64) error {
 	return enc.UInt64(math.Float64bits(v))
+}
+
+func (enc *Encoder) Float64Nullable(v float64) error {
+	if err := enc.Nullable(); err != nil {
+		return err
+	}
+	return enc.Float64(v)
 }
 
 func (enc *Encoder) String(v string) error {
@@ -112,6 +203,13 @@ func (enc *Encoder) String(v string) error {
 	return nil
 }
 
+func (enc *Encoder) StringNullable(v string) error {
+	if err := enc.Nullable(); err != nil {
+		return err
+	}
+	return enc.String(v)
+}
+
 func (enc *Encoder) RawString(str []byte) error {
 	if err := enc.Uvarint(uint64(len(str))); err != nil {
 		return err
@@ -120,6 +218,13 @@ func (enc *Encoder) RawString(str []byte) error {
 		return err
 	}
 	return nil
+}
+
+func (enc *Encoder) RawStringNullable(str []byte) error {
+	if err := enc.Nullable(); err != nil {
+		return err
+	}
+	return enc.RawString(str)
 }
 
 func (enc *Encoder) Write(b []byte) (int, error) {
--- lib/data/block_write_column.go
+++ lib/data/block_write_column.go
@@ -12,48 +12,96 @@ func (block *Block) WriteDate(c int, v time.Time) error {
 	return block.buffers[c].Column.UInt16(uint16(v.Unix() / 24 / 3600))
 }
 
+func (block *Block) WriteDateNullable(c int, v time.Time) error {
+	return block.buffers[c].Column.UInt16Nullable(uint16(v.Unix() / 24 / 3600))
+}
+
 func (block *Block) WriteDateTime(c int, v time.Time) error {
 	return block.buffers[c].Column.UInt32(uint32(v.Unix()))
+}
+
+func (block *Block) WriteDateTimeNullable(c int, v time.Time) error {
+	return block.buffers[c].Column.UInt32Nullable(uint32(v.Unix()))
 }
 
 func (block *Block) WriteInt8(c int, v int8) error {
 	return block.buffers[c].Column.Int8(v)
 }
 
+func (block *Block) WriteInt8Nullable(c int, v int8) error {
+	return block.buffers[c].Column.Int8Nullable(v)
+}
+
 func (block *Block) WriteInt16(c int, v int16) error {
 	return block.buffers[c].Column.Int16(v)
+}
+
+func (block *Block) WriteInt16Nullable(c int, v int16) error {
+	return block.buffers[c].Column.Int16Nullable(v)
 }
 
 func (block *Block) WriteInt32(c int, v int32) error {
 	return block.buffers[c].Column.Int32(v)
 }
 
+func (block *Block) WriteInt32Nullable(c int, v int32) error {
+	return block.buffers[c].Column.Int32Nullable(v)
+}
+
 func (block *Block) WriteInt64(c int, v int64) error {
 	return block.buffers[c].Column.Int64(v)
+}
+
+func (block *Block) WriteInt64Nullable(c int, v int64) error {
+	return block.buffers[c].Column.Int64Nullable(v)
 }
 
 func (block *Block) WriteUInt8(c int, v uint8) error {
 	return block.buffers[c].Column.UInt8(v)
 }
 
+func (block *Block) WriteUInt8Nullable(c int, v uint8) error {
+	return block.buffers[c].Column.UInt8Nullable(v)
+}
+
 func (block *Block) WriteUInt16(c int, v uint16) error {
 	return block.buffers[c].Column.UInt16(v)
+}
+
+func (block *Block) WriteUInt16Nullable(c int, v uint16) error {
+	return block.buffers[c].Column.UInt16Nullable(v)
 }
 
 func (block *Block) WriteUInt32(c int, v uint32) error {
 	return block.buffers[c].Column.UInt32(v)
 }
 
+func (block *Block) WriteUInt32Nullable(c int, v uint32) error {
+	return block.buffers[c].Column.UInt32Nullable(v)
+}
+
 func (block *Block) WriteUInt64(c int, v uint64) error {
 	return block.buffers[c].Column.UInt64(v)
+}
+
+func (block *Block) WriteUInt64Nullable(c int, v uint64) error {
+	return block.buffers[c].Column.UInt64Nullable(v)
 }
 
 func (block *Block) WriteFloat32(c int, v float32) error {
 	return block.buffers[c].Column.Float32(v)
 }
 
+func (block *Block) WriteFloat32Nullable(c int, v float32) error {
+	return block.buffers[c].Column.Float32Nullable(v)
+}
+
 func (block *Block) WriteFloat64(c int, v float64) error {
 	return block.buffers[c].Column.Float64(v)
+}
+
+func (block *Block) WriteFloat64Nullable(c int, v float64) error {
+	return block.buffers[c].Column.Float64Nullable(v)
 }
 
 func (block *Block) WriteBytes(c int, v []byte) error {
@@ -66,6 +114,13 @@ func (block *Block) WriteBytes(c int, v []byte) error {
 	return nil
 }
 
+func (block *Block) WriteBytesNullable(c int, v []byte) error {
+	if err := block.buffers[c].Column.Nullable(); err != nil {
+		return err
+	}
+	return block.WriteBytes(c, v)
+}
+
 func (block *Block) WriteString(c int, v string) error {
 	if err := block.buffers[c].Column.Uvarint(uint64(len(v))); err != nil {
 		return err
@@ -76,7 +131,21 @@ func (block *Block) WriteString(c int, v string) error {
 	return nil
 }
 
+func (block *Block) WriteStringNullable(c int, v string) error {
+	if err := block.buffers[c].Column.Nullable(); err != nil {
+		return err
+	}
+	return block.WriteString(c, v)
+}
+
 func (block *Block) WriteFixedString(c int, v []byte) error {
+	return block.Columns[c].Write(block.buffers[c].Column, v)
+}
+
+func (block *Block) WriteFixedStringNullable(c int, v []byte) error {
+	if err := block.buffers[c].Column.Nullable(); err != nil {
+		return err
+	}
 	return block.Columns[c].Write(block.buffers[c].Column, v)
 }
 
@@ -87,4 +156,11 @@ func (block *Block) WriteArray(c int, v *types.Array) error {
 	}
 	block.offsets[c] += ln
 	return block.buffers[c].Offset.UInt64(block.offsets[c])
+}
+
+func (block *Block) WriteArrayNullable(c int, v *types.Array) error {
+	if err := block.buffers[c].Column.Nullable(); err != nil {
+		return err
+	}
+	return block.WriteArray(c, v)
 }
