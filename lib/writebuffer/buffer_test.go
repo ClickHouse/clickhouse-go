d37b9e97762897eeb46dcff4b23054b7f9716839
Chao Wang <chaowang@uber.com>
2019-03-09 22:37:03 -0500

    Fix lint

--- lib/writebuffer/buffer_test.go
+++ lib/writebuffer/buffer_test.go
@@ -10,7 +10,13 @@ import (
 func Test_WriteBuffer_SafeWithLeakyPool(t *testing.T) {
 	leakypool.InitBytePool(1)
 	wb := New(InitialSize)
-	wb.Write(make([]byte, 1))
+	n, err := wb.Write(make([]byte, 1))
+	assert.NoError(t, err)
+	assert.Equal(t, 1, n)
 	leakypool.PutBytes(make([]byte, InitialSize))
-	assert.NotPanics(t, func() { wb.Write(make([]byte, InitialSize+1)) })
+	assert.NotPanics(t, func() {
+		n, err = wb.Write(make([]byte, InitialSize+1))
+		assert.Equal(t, InitialSize+1, n)
+		assert.NoError(t, err)
+	})
 }
