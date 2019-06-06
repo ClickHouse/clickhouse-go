00ce8d00ff1d601c961a3152f9ddd62ccfa3b941
John Troy <john@noxi.us>
2019-06-06 11:21:55 -0400

    Fix error message to print the correct header byte.

--- lib/binary/compress_reader.go
+++ lib/binary/compress_reader.go
@@ -103,7 +103,7 @@ func (cr *compressReader) readCompressedData() (err error) {
 			return
 		}
 	} else {
-		return fmt.Errorf("Unknown compression method: %d ", cr.header[0])
+		return fmt.Errorf("Unknown compression method: %d ", cr.header[16])
 	}
 
 	return nil
