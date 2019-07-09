ClickHouse/clickhouse-go

commit 304bf97070cc29f81d6992f4870ab401710e38d7

Support IPv4 and IPv6 data types
For insertion, handling of net.IP is taken over by new column types IPv4
and IPv6. Values of type net.IP can no longer be inserted into columns
of type FixedString(16).

For selection, columns of type IPv4 and IPv6 are returned as values with
type net.IP.

lib/column/ipv4.go
@@ -1,0 +1,33 @@
+package column
+
+import (
+	"net"
+
+	"github.com/kshvakov/clickhouse/lib/binary"
+)
+
+type IPv4 struct {
+	base
+}
+
+func (*IPv4) Read(decoder *binary.Decoder) (interface{}, error) {
+	v, err := decoder.Fixed(4)
+	if err != nil {
+		return nil, err
+	}
+	return net.IPv4(v[0], v[1], v[2], v[3]), nil
+}
+
+func (ip *IPv4) Write(encoder *binary.Encoder, v interface{}) error {
+	netIP, ok := v.(net.IP)
+	if !ok {
+		return &ErrUnexpectedType{
+			T:      v,
+			Column: ip,
+		}
+	}
+	if _, err := encoder.Write([]byte(netIP.To4())); err != nil {
+		return err
+	}
+	return nil
+}

clickhouse.go
@@ -185,7 +185,7 @@ func (ch *clickhouse) CheckNamedValue(nv *driver.NamedValue) error {
 		[]string:
 		return nil
 	case net.IP:
-		nv.Value = column.IP(v)
+		return nil
 	case driver.Valuer:
 		value, err := v.Value()
 		if err != nil {

clickhouse_test.go
@@ -1026,8 +1026,10 @@ func Test_IP(t *testing.T) {
 	const (
 		ddl = `
 			CREATE TABLE clickhouse_test_ip (
-				IPv4 FixedString(16),
-				IPv6 FixedString(16)
+				OldIPv4 FixedString(16),
+				OldIPv6 FixedString(16),
+				IPv4    IPv4,
+				IPv6    IPv6
 			) Engine=Memory;
 		`
 	)
@@ -1040,8 +1042,8 @@ func Test_IP(t *testing.T) {
 			if _, err := connect.Exec("DROP TABLE IF EXISTS clickhouse_test_ip"); assert.NoError(t, err) {
 				if _, err := tx.Exec(ddl); assert.NoError(t, err) {
 					if tx, err := connect.Begin(); assert.NoError(t, err) {
-						if stmt, err := tx.Prepare("INSERT INTO clickhouse_test_ip VALUES(?, ?)"); assert.NoError(t, err) {
-							if _, err := stmt.Exec(column.IP(ipv4), column.IP(ipv6)); !assert.NoError(t, err) {
+						if stmt, err := tx.Prepare("INSERT INTO clickhouse_test_ip VALUES(?, ?, ?, ?)"); assert.NoError(t, err) {
+							if _, err := stmt.Exec(column.IP(ipv4), column.IP(ipv6), ipv4, ipv6); !assert.NoError(t, err) {
 								t.Fatal(err)
 							}
 						}
@@ -1049,13 +1051,17 @@ func Test_IP(t *testing.T) {
 							t.Fatal(err)
 						}
 					}
-					if rows, err := connect.Query("SELECT IPv4, IPv6 FROM clickhouse_test_ip"); assert.NoError(t, err) {
+					if rows, err := connect.Query("SELECT OldIPv4, OldIPv6, IPv4, IPv6 FROM clickhouse_test_ip"); assert.NoError(t, err) {
 						if assert.True(t, rows.Next()) {
-							var v4, v6 column.IP
-							if err := rows.Scan(&v4, &v6); assert.NoError(t, err) {
-								if assert.Equal(t, ipv4, net.IP(v4)) {
-									assert.Equal(t, ipv6, net.IP(v6))
-								}
+							var (
+								oldIPv4, oldIPv6 column.IP
+								v4, v6           net.IP
+							)
+							if err := rows.Scan(&oldIPv4, &oldIPv6, &v4, &v6); assert.NoError(t, err) {
+								assert.Equal(t, net.IP(oldIPv4), ipv4)
+								assert.Equal(t, net.IP(oldIPv6), ipv6)
+								assert.Equal(t, v4, ipv4)
+								assert.Equal(t, v6, ipv6)
 							}
 						}
 					}

lib/column/column.go
@@ -136,6 +136,20 @@ func Factory(name, chType string, timezone *time.Location) (Column, error) {
 			},
 			Timezone: timezone,
 		}, nil
+	case "IPv4":
+		return &IPv4{
+			base: base{
+				name:   name,
+				chType: chType,
+			},
+		}, nil
+	case "IPv6":
+		return &IPv6{
+			base: base{
+				name:   name,
+				chType: chType,
+			},
+		}, nil
 	}
 
 	switch {

lib/column/ipv6.go
@@ -1,0 +1,33 @@
+package column
+
+import (
+	"net"
+
+	"github.com/kshvakov/clickhouse/lib/binary"
+)
+
+type IPv6 struct {
+	base
+}
+
+func (*IPv6) Read(decoder *binary.Decoder) (interface{}, error) {
+	v, err := decoder.Fixed(16)
+	if err != nil {
+		return nil, err
+	}
+	return net.IP(v), nil
+}
+
+func (ip *IPv6) Write(encoder *binary.Encoder, v interface{}) error {
+	netIP, ok := v.(net.IP)
+	if !ok {
+		return &ErrUnexpectedType{
+			T:      v,
+			Column: ip,
+		}
+	}
+	if _, err := encoder.Write([]byte(netIP.To16())); err != nil {
+		return err
+	}
+	return nil
+}

commit 83b7ff2876c581b6812ab37b2e0930129a4287d8

Fix IPv4 endianness
Commit 304bf97070cc29f81d6992f4870ab401710e38d7 introduced IPv4
insertion and selection with the wrong endianness, which resulted in
IPv4 values inserted using this driver being backwards. This commit
makes IPv4 insertion and selection use the correct endianness.

lib/column/ipv4.go
@@ -15,7 +15,7 @@ func (*IPv4) Read(decoder *binary.Decoder) (interface{}, error) {
 	if err != nil {
 		return nil, err
 	}
-	return net.IPv4(v[0], v[1], v[2], v[3]), nil
+	return net.IPv4(v[3], v[2], v[1], v[0]), nil
 }
 
 func (ip *IPv4) Write(encoder *binary.Encoder, v interface{}) error {
@@ -26,7 +26,8 @@ func (ip *IPv4) Write(encoder *binary.Encoder, v interface{}) error {
 			Column: ip,
 		}
 	}
-	if _, err := encoder.Write([]byte(netIP.To4())); err != nil {
+	ip4 := netIP.To4()
+	if _, err := encoder.Write([]byte{ip4[3], ip4[2], ip4[1], ip4[0]}); err != nil {
 		return err
 	}
 	return nil