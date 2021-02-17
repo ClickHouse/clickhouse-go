f79ff695e7084389d45b4ba819626ab8f6b321dc
Robin Hahling <robin.hahling@gw-computing.net>
2021-02-17 20:47:13 +0100

    lib/column: fix base type for IPv4 and IPv6
    Using `net.IP{}` as a base type for an IPv4 is incorrect as it is not a
    valid IPv4. This can be verified by calling the `To4()` which should
    return non-nil value for an IPv4 address. To address this problem, use
    the actual zero value for an IPv4 address (`net.IPv4zero`).
    
    Similarly, use `net.IPv6unspecified` for the IPv6 type.

--- lib/column/common.go
+++ lib/column/common.go
@@ -29,8 +29,8 @@ var columnBaseTypes = map[interface{}]reflect.Value{
 	float64(0):  reflect.ValueOf(float64(0)),
 	string(""):  reflect.ValueOf(string("")),
 	time.Time{}: reflect.ValueOf(time.Time{}),
-	IPv4{}:      reflect.ValueOf(net.IP{}),
-	IPv6{}:      reflect.ValueOf(net.IP{}),
+	IPv4{}:      reflect.ValueOf(net.IPv4zero),
+	IPv6{}:      reflect.ValueOf(net.IPv6unspecified),
 }
 
 var arrayBaseTypes = map[interface{}]reflect.Type{
@@ -46,8 +46,8 @@ var arrayBaseTypes = map[interface{}]reflect.Type{
 	float64(0):  reflect.ValueOf(float64(0)).Type(),
 	string(""):  reflect.ValueOf(string("")).Type(),
 	time.Time{}: reflect.ValueOf(time.Time{}).Type(),
-	IPv4{}:      reflect.ValueOf(net.IP{}).Type(),
-	IPv6{}:      reflect.ValueOf(net.IP{}).Type(),
+	IPv4{}:      reflect.ValueOf(net.IPv4zero).Type(),
+	IPv6{}:      reflect.ValueOf(net.IPv6unspecified).Type(),
 }
 
 type base struct {
