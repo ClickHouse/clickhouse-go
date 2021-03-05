2d21528af4fe8cd41c601dee3e34040d990ca4ba
Thibault Deutsch <thibault@arista.com>
2021-03-05 14:18:33 +0000

    fix: unsafe conversion from string to []bytes
    The previous code was pointing the SliceHeader to the string's header.
    This is wrong because they aren't the same size, SliceHeader also
    contains the cap. The code was overwriting the cap value to be equal to
    the len of str, but this cause issue when str is allocated on the stack.
    In that case, it will overwrite the return address of the function.
    
    Instead of fixing the original code, I have copied a well-documented and
    tested version from m3db (License Apache 2).

--- lib/binary/encoder.go
+++ lib/binary/encoder.go
@@ -164,8 +164,31 @@ type WriteFlusher interface {
 }
 
 func Str2Bytes(str string) []byte {
-	header := (*reflect.SliceHeader)(unsafe.Pointer(&str))
-	header.Len = len(str)
-	header.Cap = header.Len
-	return *(*[]byte)(unsafe.Pointer(header))
+	// Copied from https://github.com/m3db/m3/blob/master/src/x/unsafe/string.go#L62
+	if len(str) == 0 {
+		return nil
+	}
+
+	// We need to declare a real byte slice so internally the compiler
+	// knows to use an unsafe.Pointer to keep track of the underlying memory so that
+	// once the slice's array pointer is updated with the pointer to the string's
+	// underlying bytes, the compiler won't prematurely GC the memory when the string
+	// goes out of scope.
+	var b []byte
+	byteHeader := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+
+	// This makes sure that even if GC relocates the string's underlying
+	// memory after this assignment, the corresponding unsafe.Pointer in the internal
+	// slice struct will be updated accordingly to reflect the memory relocation.
+	byteHeader.Data = (*reflect.StringHeader)(unsafe.Pointer(&str)).Data
+
+	// It is important that we access str after we assign the Data
+	// pointer of the string header to the Data pointer of the slice header to
+	// make sure the string (and the underlying bytes backing the string) don't get
+	// GC'ed before the assignment happens.
+	l := len(str)
+	byteHeader.Len = l
+	byteHeader.Cap = l
+
+	return b
 }
