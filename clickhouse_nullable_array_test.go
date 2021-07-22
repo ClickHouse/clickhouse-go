f8507cb815f904ed76ba09f8b07c7f8cbcbf738a
vl4deee11 <boi4enkovlad@yandex>
2021-07-22 21:22:20 +0100

    added nullable enums

--- clickhouse_nullable_array_test.go
+++ clickhouse_nullable_array_test.go
@@ -31,51 +31,55 @@ func Test_NullableArray(t *testing.T) {
 				arr_string     Array(Nullable(String)),
 				arr_arr_string Array(Array(Nullable(String))),
 				arr_date       Array(Nullable(Date)),
-				arr_datetime   Array(Nullable(DateTime))
+				arr_datetime   Array(Nullable(DateTime)),
+				arr_enum8_str  Array(Nullable(Enum8('a8' = 1, 'b8' = 2))),
+				arr_enum8_int  Array(Nullable(Enum8('a8' = 1, 'b8' = 2))),
+				arr_enum16_str Array(Nullable(Enum16('a16' = 1, 'b16' = 2))),
+				arr_enum16_int Array(Nullable(Enum16('a16' = 1, 'b16' = 2)))
 			) Engine = Memory;
 		`
 		dml = `
-			INSERT INTO clickhouse_test_nullable_array (
-				arr_decimal,
-				arr_int8,
-				arr_int16,
-				arr_int32,
-				arr_int64,
-
-				arr_uint8,
-				arr_uint16,
-				arr_uint32,
-				arr_uint64,
-			                                            
-				arr_float32,
-				arr_float64,
-			                                            
-				arr_ipv6,
-				arr_ipv4,
-			                                            
-				arr_string,
-			    arr_arr_string,
-			                                            
-				arr_date,
-				arr_datetime
-			) VALUES (
-			    ?,
-				?,
-				?,
-				?,
-				?,
-				?,
-				?,
-				?,
-				?,
-				?,
-				?,
-				?,
-				?,
-				?,
-				?,
-				?
-			)
+			INSERT INTO clickhouse_test_nullable_array (arr_decimal,
+														arr_int8,
+														arr_int16,
+														arr_int32,
+														arr_int64,
+														arr_uint8,
+														arr_uint16,
+														arr_uint32,
+														arr_uint64,
+														arr_float32,
+														arr_float64,
+														arr_ipv6,
+														arr_ipv4,
+														arr_string,
+														arr_arr_string,
+														arr_date,
+														arr_datetime,
+														arr_enum8_str,
+														arr_enum8_int,
+														arr_enum16_str,
+														arr_enum16_int)
+			VALUES (?,
+					?,
+					?,
+					?,
+					?,
+					?,
+					?,
+					?,
+					?,
+					?,
+					?,
+					?,
+					?,
+					?,
+					?,
+					?,
+					?,
+					?,
+					?,
+					?);
 		`
 		query = `
 			SELECT
@@ -107,16 +111,28 @@ func Test_NullableArray(t *testing.T) {
 	timeV, _ := time.Parse("2006-01-02 15:04:05", "2021-07-11 00:00:00")
 	dateV, _ := time.Parse("2006-01-02", "2021-07-11")
 
+	enum8VA := "a8"
+	enum8VB := "b8"
+
+	enum8V1 := int8(1)
+	enum8V2 := int8(2)
+
+	enum16VA := "a16"
+	enum16VB := "b16"
+
+	enum16V1 := int16(1)
+	enum16V2 := int16(2)
+
 	var timeNil *time.Time
 
-	if connect, err := sql.Open("clickhouse", "tcp://127.0.0.1:9000?debug=true"); assert.NoError(t, err) {
+	if connect, err := sql.Open("clickhouse", "tcp://0.0.0.0:9000?debug=true"); assert.NoError(t, err) {
 		if tx, err := connect.Begin(); assert.NoError(t, err) {
 			if _, err := connect.Exec("DROP TABLE IF EXISTS clickhouse_test_nullable_array"); assert.NoError(t, err) {
 				if _, err := tx.Exec(ddl); assert.NoError(t, err) {
 					if tx, err := connect.Begin(); assert.NoError(t, err) {
 						stmt, err := tx.Prepare(dml)
 						if assert.NoError(t, err) {
-							for i := 0; i < 100; i++ {
+							for i := 0; i < 10; i++ {
 								if _, err := stmt.Exec(
 									[]*float64{&decV, nil, &decV},
 									[]*int8{&int8V, nil, &int8V},
@@ -140,6 +156,11 @@ func Test_NullableArray(t *testing.T) {
 
 									[]*time.Time{&dateV, nil, &dateV},
 									[]*time.Time{&timeV, nil, &timeV},
+
+									[]*string{&enum8VA, nil, &enum8VB},
+									[]*int8{&enum8V1, nil, &enum8V2},
+									[]*string{&enum16VA, nil, &enum16VB},
+									[]*int16{&enum16V1, nil, &enum16V2},
 								); !assert.NoError(t, err) {
 									t.Fatal(err)
 								}
@@ -152,23 +173,27 @@ func Test_NullableArray(t *testing.T) {
 					if rows, err := connect.Query(query); assert.NoError(t, err) {
 						for rows.Next() {
 							var (
-								ArrDecimal   = make([]*int64, 0)
-								ArrInt8      = make([]*int8, 0)
-								ArrInt16     = make([]*int16, 0)
-								ArrInt32     = make([]*int32, 0)
-								ArrInt64     = make([]*int64, 0)
-								ArrUInt8     = make([]*uint8, 0)
-								ArrUInt16    = make([]*uint16, 0)
-								ArrUInt32    = make([]*uint32, 0)
-								ArrUInt64    = make([]*uint64, 0)
-								ArrFloat32   = make([]*float32, 0)
-								ArrFloat64   = make([]*float64, 0)
-								ArrIpv6      = make([]*net.IP, 0)
-								ArrIpv4      = make([]*net.IP, 0)
-								ArrString    = make([]*string, 0)
-								ArrArrString = make([][]*string, 0)
-								ArrDate      = make([]*time.Time, 0)
-								ArrDateTime  = make([]*time.Time, 0)
+								ArrDecimal     = make([]*int64, 0)
+								ArrInt8        = make([]*int8, 0)
+								ArrInt16       = make([]*int16, 0)
+								ArrInt32       = make([]*int32, 0)
+								ArrInt64       = make([]*int64, 0)
+								ArrUInt8       = make([]*uint8, 0)
+								ArrUInt16      = make([]*uint16, 0)
+								ArrUInt32      = make([]*uint32, 0)
+								ArrUInt64      = make([]*uint64, 0)
+								ArrFloat32     = make([]*float32, 0)
+								ArrFloat64     = make([]*float64, 0)
+								ArrIpv6        = make([]*net.IP, 0)
+								ArrIpv4        = make([]*net.IP, 0)
+								ArrString      = make([]*string, 0)
+								ArrArrString   = make([][]*string, 0)
+								ArrDate        = make([]*time.Time, 0)
+								ArrDateTime    = make([]*time.Time, 0)
+								ArrEnum8Str    = make([]*string, 0)
+								ArrEnum8Int8   = make([]*string, 0)
+								ArrEnum16Str   = make([]*string, 0)
+								ArrEnum16Int16 = make([]*string, 0)
 							)
 							if err := rows.Scan(
 								&ArrDecimal,
@@ -188,6 +213,10 @@ func Test_NullableArray(t *testing.T) {
 								&ArrArrString,
 								&ArrDate,
 								&ArrDateTime,
+								&ArrEnum8Str,
+								&ArrEnum8Int8,
+								&ArrEnum16Str,
+								&ArrEnum16Int16,
 							); assert.NoError(t, err) {
 								assert.Equal(t, ArrDecimal, []*int64{&int64Dec, nil, &int64Dec})
 								assert.Equal(t, ArrInt8, []*int8{&int8V, nil, &int8V})
@@ -213,6 +242,11 @@ func Test_NullableArray(t *testing.T) {
 								assert.True(t, len(ArrDateTime) == 3)
 								assert.Equal(t, ArrDate[1], timeNil)
 								assert.Equal(t, ArrDateTime[1], timeNil)
+
+								assert.Equal(t, ArrEnum8Int8, []*string{&enum8VA, nil, &enum8VB})
+								assert.Equal(t, ArrEnum16Int16, []*string{&enum16VA, nil, &enum16VB})
+								assert.Equal(t, ArrEnum8Str, []*string{&enum8VA, nil, &enum8VB})
+								assert.Equal(t, ArrEnum16Str, []*string{&enum16VA, nil, &enum16VB})
 							}
 						}
 					}
