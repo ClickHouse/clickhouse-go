8f3d1a891036e1e6b901fa486fee7f279387272e
kshvakov <shvakov@gmail.com>
2022-01-27 21:56:07 +0200

    Add test: https://github.com/ClickHouse/clickhouse-go/pull/470

---
+++ tests/issues/470_pr_test.go
@@ -0,0 +1,31 @@
+package issues
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test470PR(t *testing.T) {
+	if conn, err := sql.Open("clickhouse", "clickhouse://127.0.0.1:9000"); assert.NoError(t, err) {
+		const ddl = `
+		CREATE TABLE issue_470_pr (
+			  Col1 Array(String)
+		) Engine Memory
+		`
+		if _, err := conn.Exec("DROP TABLE IF EXISTS issue_470_pr"); assert.NoError(t, err) {
+			if _, err := conn.Exec(ddl); assert.NoError(t, err) {
+				scope, err := conn.Begin()
+				if !assert.NoError(t, err) {
+					return
+				}
+				if batch, err := scope.Prepare("INSERT INTO issue_470_pr"); assert.NoError(t, err) {
+					if _, err := batch.Exec(nil); assert.Error(t, err) {
+						assert.Contains(t, err.Error(), "converting <nil> to Array(String) is unsupported")
+					}
+				}
+			}
+		}
+	}
+}
