f6309547df6c9378806f0b3e94379177d919d2a4
Dale Mcdiarmid <dale@clickhouse.com>
2022-07-11 12:02:05 +0100

    Add zstd tests

---
+++ tests/compression_test.go
@@ -0,0 +1,59 @@
+package tests
+
+import (
+	"context"
+	"github.com/ClickHouse/clickhouse-go/v2"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestZSTDCompression(t *testing.T) {
+	var (
+		ctx       = context.Background()
+		conn, err = clickhouse.Open(&clickhouse.Options{
+			Addr: []string{"127.0.0.1:9000"},
+			Auth: clickhouse.Auth{
+				Database: "default",
+				Username: "default",
+				Password: "",
+			},
+			Compression: &clickhouse.Compression{
+				Method: clickhouse.CompressionZSTD,
+			},
+			MaxOpenConns: 1,
+		})
+	)
+	if assert.NoError(t, err) {
+		const ddl = `
+		CREATE TABLE test_array (
+			  Col1 Array(String)
+		) Engine Memory
+		`
+		defer func() {
+			conn.Exec(ctx, "DROP TABLE test_array")
+		}()
+		require.NoError(t, conn.Exec(ctx, ddl))
+		batch, err := conn.PrepareBatch(ctx, "INSERT INTO test_array")
+		require.NoError(t, err)
+		var (
+			col1Data = []string{"A", "b", "c"}
+		)
+		for i := 0; i < 10; i++ {
+			require.NoError(t, batch.Append(col1Data))
+		}
+		require.NoError(t, batch.Send())
+		rows, err := conn.Query(ctx, "SELECT * FROM test_array")
+		require.NoError(t, err)
+		for rows.Next() {
+			var (
+				col1 []string
+			)
+			require.NoError(t, rows.Scan(&col1))
+			assert.Equal(t, col1Data, col1)
+		}
+		require.NoError(t, rows.Close())
+		require.NoError(t, rows.Err())
+
+	}
+}
--- tests/std/compression_test.go
+++ tests/std/compression_test.go
@@ -66,6 +66,62 @@ func TestCompressionHttpStd(t *testing.T) {
 	}
 }
 
+func TestZSTDCompressionHttpStd(t *testing.T) {
+	protocols := map[clickhouse.Protocol]int{clickhouse.HTTP: 8123, clickhouse.Native: 9000}
+	for protocol, port := range protocols {
+		conn := clickhouse.OpenDB(&clickhouse.Options{
+			Addr: []string{fmt.Sprintf("127.0.0.1:%d", port)},
+			Auth: clickhouse.Auth{
+				Database: "default",
+				Username: "default",
+				Password: "",
+			},
+			Settings: clickhouse.Settings{
+				"max_execution_time": 60,
+			},
+			DialTimeout: 5 * time.Second,
+			Compression: &clickhouse.Compression{
+				Method: clickhouse.CompressionZSTD,
+			},
+			Protocol: protocol,
+		})
+		conn.Exec("DROP TABLE IF EXISTS test_array_compress")
+		const ddl = `
+		CREATE TABLE test_array_compress (
+			  Col1 Array(String)
+		) Engine Memory
+		`
+		defer func() {
+			conn.Exec("DROP TABLE test_array_compress")
+		}()
+		_, err := conn.Exec(ddl)
+		require.NoError(t, err)
+		scope, err := conn.Begin()
+		require.NoError(t, err)
+		batch, err := scope.Prepare("INSERT INTO test_array_compress")
+		require.NoError(t, err)
+		var (
+			col1Data = []string{"A", "b", "c"}
+		)
+		for i := 0; i < 100; i++ {
+			_, err := batch.Exec(col1Data)
+			require.NoError(t, err)
+		}
+		require.NoError(t, scope.Commit())
+		rows, err := conn.Query("SELECT * FROM test_array_compress")
+		require.NoError(t, err)
+		for rows.Next() {
+			var (
+				col1 interface{}
+			)
+			require.NoError(t, rows.Scan(&col1))
+			assert.Equal(t, col1Data, col1)
+		}
+		require.NoError(t, rows.Close())
+		require.NoError(t, rows.Err())
+	}
+}
+
 //test compression over std with dsn and compress
 
 func TestCompressionHttpStdDSN(t *testing.T) {
