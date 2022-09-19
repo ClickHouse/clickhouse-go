f1763ceb858313eb4e543104a2b28d86cb241192
Dale Mcdiarmid <dale@clickhouse.com>
2022-09-19 17:17:01 +0100

    sql.NullString test

--- tests/std/string_test.go
+++ tests/std/string_test.go
@@ -18,6 +18,7 @@
 package std
 
 import (
+	"database/sql"
 	"fmt"
 	clickhouse_tests "github.com/ClickHouse/clickhouse-go/v2/tests"
 	"github.com/stretchr/testify/assert"
@@ -41,7 +42,7 @@ func TestSimpleStdString(t *testing.T) {
 		t.Run(fmt.Sprintf("%s Protocol", name), func(t *testing.T) {
 			conn, err := GetConnectionFromDSN(dsn)
 			require.NoError(t, err)
-			const ddl = `CREATE TABLE test_array (Col1 String) Engine MergeTree() ORDER BY tuple()`
+			const ddl = `CREATE TABLE test_array (Col1 String, Col2 Nullable(String)) Engine MergeTree() ORDER BY tuple()`
 			conn.Exec("DROP TABLE test_array")
 			defer func() {
 				conn.Exec("DROP TABLE test_array")
@@ -56,7 +57,7 @@ func TestSimpleStdString(t *testing.T) {
 				col1Data = "A"
 			)
 			for i := 0; i < 10; i++ {
-				_, err := batch.Exec(col1Data)
+				_, err := batch.Exec(col1Data, nil)
 				require.NoError(t, err)
 			}
 			require.NoError(t, scope.Commit())
@@ -65,9 +66,11 @@ func TestSimpleStdString(t *testing.T) {
 			for rows.Next() {
 				var (
 					col1 interface{}
+					col2 sql.NullString
 				)
-				require.NoError(t, rows.Scan(&col1))
+				require.NoError(t, rows.Scan(&col1, &col2))
 				assert.Equal(t, col1Data, col1)
+				assert.Equal(t, sql.NullString{Valid: false}, col2)
 			}
 			require.NoError(t, rows.Close())
 			require.NoError(t, rows.Err())
