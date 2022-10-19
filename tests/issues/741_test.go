957f236daf23d80aaf52d80e6cd6ef14cad28b77
Dale Mcdiarmid <dale@clickhouse.com>
2022-10-19 15:38:55 +0100

    test append behaviour

--- tests/issues/741_test.go
+++ tests/issues/741_test.go
@@ -1,6 +1,7 @@
 package issues
 
 import (
+	"context"
 	"fmt"
 	"github.com/ClickHouse/clickhouse-go/v2"
 	clickhouse_tests "github.com/ClickHouse/clickhouse-go/v2/tests"
@@ -58,18 +59,13 @@ func TestIssue741SingleColumn(t *testing.T) {
 	require.NoError(t, err)
 }
 
-func TestIssue741RandomOrder(t *testing.T) {
+func generateRandomInsert(tableName string) (string, string, []interface{}) {
 	columns := map[string]interface{}{
 		"Col1 String": "a",
 		"Col2 Int64":  int64(1),
 		"Col3 Int32":  int32(2),
 		"Col4 Bool":   true,
 	}
-	useSSL, err := strconv.ParseBool(clickhouse_tests.GetEnv("CLICKHOUSE_USE_SSL", "false"))
-	require.NoError(t, err)
-	conn, err := clickhouse_std_tests.GetDSNConnection("issues", clickhouse.Native, useSSL, "false")
-	require.NoError(t, err)
-	conn.Exec("DROP TABLE IF EXISTS issue_741_random")
 	colNames := make([]string, len(columns))
 	i := 0
 	for k := range columns {
@@ -79,15 +75,10 @@ func TestIssue741RandomOrder(t *testing.T) {
 	// shuffle our columns for ddl
 	rand.Shuffle(len(colNames), func(i, j int) { colNames[i], colNames[j] = colNames[j], colNames[i] })
 	ddl := fmt.Sprintf(`
-			CREATE TABLE issue_741_random (
+			CREATE TABLE %s (
 				%s
 			)
-			Engine MergeTree() ORDER BY tuple()`, strings.Join(colNames, ", "))
-	_, err = conn.Exec(ddl)
-	require.NoError(t, err)
-	defer func() {
-		conn.Exec("DROP TABLE issue_741_random")
-	}()
+			Engine MergeTree() ORDER BY tuple()`, tableName, strings.Join(colNames, ", "))
 	// shuffle our columns for insert
 	rand.Shuffle(len(colNames), func(i, j int) { colNames[i], colNames[j] = colNames[j], colNames[i] })
 	names := make([]string, len(colNames))
@@ -96,14 +87,48 @@ func TestIssue741RandomOrder(t *testing.T) {
 		names[i] = strings.Fields(colNames[i])[0]
 		placeholders[i] = "?"
 	}
-	stmt, err := conn.Prepare(fmt.Sprintf("INSERT INTO issue_741_random (%s) VALUES (%s)", strings.Join(names, ", "), strings.Join(placeholders, ", ")))
-	require.NoError(t, err)
+	insertStatement := fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s)", tableName, strings.Join(names, ", "), strings.Join(placeholders, ", "))
 	values := make([]interface{}, len(colNames))
 	for i, colName := range colNames {
 		values[i] = columns[colName]
 	}
+	return ddl, insertStatement, values
+}
+
+func TestIssue741RandomOrder(t *testing.T) {
+	useSSL, err := strconv.ParseBool(clickhouse_tests.GetEnv("CLICKHOUSE_USE_SSL", "false"))
+	require.NoError(t, err)
+	conn, err := clickhouse_std_tests.GetDSNConnection("issues", clickhouse.Native, useSSL, "false")
+	require.NoError(t, err)
+	conn.Exec("DROP TABLE IF EXISTS issue_741_random")
+	defer func() {
+		conn.Exec("DROP TABLE issue_741_random")
+	}()
+	ddl, insertStatement, values := generateRandomInsert("issue_741_random")
+	_, err = conn.Exec(ddl)
+	require.NoError(t, err)
+	stmt, err := conn.Prepare(fmt.Sprintf(insertStatement))
+	require.NoError(t, err)
 	_, err = stmt.Exec(values...)
 	require.NoError(t, err)
 }
 
-// test Append
+// test Append on native connection
+func TestIssue741Append(t *testing.T) {
+	var (
+		conn, err = clickhouse_tests.GetConnection("issues", clickhouse.Settings{
+			"max_execution_time": 60,
+		}, nil, &clickhouse.Compression{
+			Method: clickhouse.CompressionLZ4,
+		})
+	)
+	ctx := context.Background()
+	require.NoError(t, err)
+	conn.Exec(ctx, "DROP TABLE IF EXISTS issue_741_append_random")
+	ddl, insertStatement, values := generateRandomInsert("issue_741_append_random")
+	require.NoError(t, conn.Exec(ctx, ddl))
+	batch, err := conn.PrepareBatch(ctx, insertStatement)
+	require.NoError(t, err)
+	require.NoError(t, batch.Append(values...))
+	require.NoError(t, batch.Send())
+}
