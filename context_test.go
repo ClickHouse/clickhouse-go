cef7fb740559d4c6780c6b4ad8c3ae022f7d48e0
Aaron Harlap <aaron@chronosphere.io>
2022-12-22 12:00:24 -0500

    Address feedback.

--- context_test.go
+++ context_test.go
@@ -8,22 +8,28 @@ import (
 )
 
 func TestContext(t *testing.T) {
-	// Call context multiple times making sure query options are persisted across calls.
-	ctx := Context(context.Background(), WithQueryID("a"))
-	ctx = Context(ctx, WithQuotaKey("b"))
-	ctx = Context(ctx, WithSettings(Settings{
-		"c": "d",
-	}))
+	t.Run("call context multiple times making sure query options are persisted across calls",
+		func(t *testing.T) {
+			ctx := Context(context.Background(), WithQueryID("a"))
+			ctx = Context(ctx, WithQuotaKey("b"))
+			ctx = Context(ctx, WithSettings(Settings{
+				"c": "d",
+			}))
 
-	opts := queryOptions(ctx)
-	require.Equal(t, "a", opts.queryID)
-	require.Equal(t, "b", opts.quotaKey)
-	require.Equal(t, "d", opts.settings["c"])
+			opts := queryOptions(ctx)
+			require.Equal(t, "a", opts.queryID)
+			require.Equal(t, "b", opts.quotaKey)
+			require.Equal(t, "d", opts.settings["c"])
+		},
+	)
 
-	// Call context multiple times with the same query options, making sure the latest is persisted.
-	ctx = Context(context.Background(), WithQueryID("a"))
-	ctx = Context(ctx, WithQueryID("b"))
+	t.Run("call context multiple times making sure query options are persisted across calls",
+		func(t *testing.T) {
+			ctx := Context(context.Background(), WithQueryID("a"))
+			ctx = Context(ctx, WithQueryID("b"))
 
-	opts = queryOptions(ctx)
-	require.Equal(t, "b", opts.queryID)
+			opts := queryOptions(ctx)
+			require.Equal(t, "b", opts.queryID)
+		},
+	)
 }
