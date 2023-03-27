e77352cefd3706b5f019a4f04726e907d78815fc
Zhehao Wu <crisismaple@vip.qq.com>
2023-03-27 23:31:29 +0800

    update test case

--- tests/std/external_table_test.go
+++ tests/std/external_table_test.go
@@ -19,7 +19,6 @@ package std
 
 import (
 	"context"
-	"crypto/tls"
 	"fmt"
 	clickhouse_tests "github.com/ClickHouse/clickhouse-go/v2/tests"
 	"github.com/stretchr/testify/require"
@@ -33,104 +32,55 @@ import (
 )
 
 func TestStdExternalTable(t *testing.T) {
-	table1, err := ext.NewTable("external_table_1",
-		ext.Column("col1", "UInt8"),
-		ext.Column("col2", "String"),
-		ext.Column("col3", "DateTime"),
-	)
-	require.NoError(t, err)
-	for i := 0; i < 10; i++ {
-		assert.NoError(t, table1.Append(uint8(i), fmt.Sprintf("value_%d", i), time.Now()))
-	}
-	table2, err := ext.NewTable("external_table_2",
-		ext.Column("col1", "UInt8"),
-		ext.Column("col2", "String"),
-		ext.Column("col3", "DateTime"),
-	)
-	require.NoError(t, err)
-	for i := 0; i < 10; i++ {
-		assert.NoError(t, table2.Append(uint8(i), fmt.Sprintf("value_%d", i), time.Now()))
-	}
+	dsns := map[string]clickhouse.Protocol{"Native": clickhouse.Native, "Http": clickhouse.HTTP}
 	useSSL, err := strconv.ParseBool(clickhouse_tests.GetEnv("CLICKHOUSE_USE_SSL", "false"))
 	require.NoError(t, err)
-	conn, err := GetStdDSNConnection(clickhouse.Native, useSSL, nil)
-	require.NoError(t, err)
-	ctx := clickhouse.Context(context.Background(),
-		clickhouse.WithExternalTable(table1, table2),
-	)
-	rows, err := conn.QueryContext(ctx, "SELECT * FROM external_table_1")
-	require.NoError(t, err)
-	for rows.Next() {
-		var (
-			col1 uint8
-			col2 string
-			col3 time.Time
-		)
-		require.NoError(t, rows.Scan(&col1, &col2, &col3))
-		t.Logf("row: col1=%d, col2=%s, col3=%s\n", col1, col2, col3)
-	}
-	rows.Close()
+	for name, protocol := range dsns {
+		t.Run(fmt.Sprintf("%s Protocol", name), func(t *testing.T) {
+			table1, err := ext.NewTable("external_table_1",
+				ext.Column("col1", "UInt8"),
+				ext.Column("col2", "String"),
+				ext.Column("col3", "DateTime"),
+			)
+			require.NoError(t, err)
+			for i := 0; i < 10; i++ {
+				assert.NoError(t, table1.Append(uint8(i), fmt.Sprintf("value_%d", i), time.Now()))
+			}
+			table2, err := ext.NewTable("external_table_2",
+				ext.Column("col1", "UInt8"),
+				ext.Column("col2", "String"),
+				ext.Column("col3", "DateTime"),
+			)
+			require.NoError(t, err)
+			for i := 0; i < 10; i++ {
+				assert.NoError(t, table2.Append(uint8(i), fmt.Sprintf("value_%d", i), time.Now()))
+			}
+			require.NoError(t, err)
+			conn, err := GetStdDSNConnection(protocol, useSSL, nil)
+			require.NoError(t, err)
+			ctx := clickhouse.Context(context.Background(),
+				clickhouse.WithExternalTable(table1, table2),
+			)
+			rows, err := conn.QueryContext(ctx, "SELECT * FROM external_table_1")
+			require.NoError(t, err)
+			for rows.Next() {
+				var (
+					col1 uint8
+					col2 string
+					col3 time.Time
+				)
+				require.NoError(t, rows.Scan(&col1, &col2, &col3))
+				t.Logf("row: col1=%d, col2=%s, col3=%s\n", col1, col2, col3)
+			}
+			rows.Close()
 
-	var count uint64
-	require.NoError(t, conn.QueryRowContext(ctx, "SELECT COUNT(*) FROM external_table_1").Scan(&count))
-	assert.Equal(t, uint64(10), count)
-	require.NoError(t, conn.QueryRowContext(ctx, "SELECT COUNT(*) FROM external_table_2").Scan(&count))
-	assert.Equal(t, uint64(10), count)
-	require.NoError(t, conn.QueryRowContext(ctx, "SELECT COUNT(*) FROM (SELECT * FROM external_table_1 UNION ALL SELECT * FROM external_table_2)").Scan(&count))
-	assert.Equal(t, uint64(20), count)
-}
-
-func TestStdHttpConnectionExternalTable(t *testing.T) {
-	table1, err := ext.NewTable("external_table_1",
-		ext.Column("col1", "UInt8"),
-		ext.Column("col2", "String"),
-		ext.Column("col3", "DateTime"),
-	)
-	require.NoError(t, err)
-	for i := 0; i < 10; i++ {
-		assert.NoError(t, table1.Append(uint8(i), fmt.Sprintf("value_%d", i), time.Now()))
+			var count uint64
+			require.NoError(t, conn.QueryRowContext(ctx, "SELECT COUNT(*) FROM external_table_1").Scan(&count))
+			assert.Equal(t, uint64(10), count)
+			require.NoError(t, conn.QueryRowContext(ctx, "SELECT COUNT(*) FROM external_table_2").Scan(&count))
+			assert.Equal(t, uint64(10), count)
+			require.NoError(t, conn.QueryRowContext(ctx, "SELECT COUNT(*) FROM (SELECT * FROM external_table_1 UNION ALL SELECT * FROM external_table_2)").Scan(&count))
+			assert.Equal(t, uint64(20), count)
+		})
 	}
-	table2, err := ext.NewTable("external_table_2",
-		ext.Column("col1", "UInt8"),
-		ext.Column("col2", "String"),
-		ext.Column("col3", "DateTime"),
-	)
-	require.NoError(t, err)
-	for i := 0; i < 10; i++ {
-		assert.NoError(t, table2.Append(uint8(i), fmt.Sprintf("value_%d", i), time.Now()))
-	}
-	useSSL, err := strconv.ParseBool(clickhouse_tests.GetEnv("CLICKHOUSE_USE_SSL", "false"))
-	require.NoError(t, err)
-	var tlsConfig *tls.Config
-	if useSSL {
-		tlsConfig = &tls.Config{}
-	}
-	conn, err := GetStdOpenDBConnection(clickhouse.HTTP, nil, tlsConfig, &clickhouse.Compression{
-		Method: clickhouse.CompressionZSTD,
-		Level:  3,
-	})
-	require.NoError(t, err)
-	ctx := clickhouse.Context(context.Background(),
-		clickhouse.WithExternalTable(table1, table2),
-	)
-	rows, err := conn.QueryContext(ctx, "SELECT * FROM external_table_1")
-	require.NoError(t, err)
-	for rows.Next() {
-		var (
-			col1 uint8
-			col2 string
-			col3 time.Time
-		)
-		require.NoError(t, rows.Scan(&col1, &col2, &col3))
-		t.Logf("row: col1=%d, col2=%s, col3=%s\n", col1, col2, col3)
-	}
-	rows.Close()
-
-	var count uint64
-	require.NoError(t, conn.QueryRowContext(ctx, "SELECT COUNT(*) FROM external_table_1").Scan(&count))
-	assert.Equal(t, uint64(10), count)
-	require.NoError(t, conn.QueryRowContext(ctx, "SELECT COUNT(*) FROM external_table_2").Scan(&count))
-	assert.Equal(t, uint64(10), count)
-	require.NoError(t, conn.QueryRowContext(ctx, "SELECT COUNT(*) FROM (SELECT * FROM external_table_1 UNION ALL SELECT * FROM external_table_2)").Scan(&count))
-	assert.Equal(t, uint64(20), count)
 }
