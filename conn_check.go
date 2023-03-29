d15722cfc1e4d810708a6936caffc4ce7da0d903
Kuba Kaflik <kuba.kaflik@clickhouse.com>
2023-03-29 09:53:02 +0200

    Use protocol's ping to check connection is live

--- conn_check.go
+++ conn_check.go
@@ -15,46 +15,18 @@
 // specific language governing permissions and limitations
 // under the License.
 
-//go:build linux || darwin || dragonfly || freebsd || netbsd || openbsd || solaris || illumos
-// +build linux darwin dragonfly freebsd netbsd openbsd solaris illumos
-
 package clickhouse
 
 import (
-	"errors"
-	"io"
-	"syscall"
+	"context"
+	"time"
 )
 
 func (c *connect) connCheck() error {
-	var sysErr error
-	sysConn, ok := c.conn.(syscall.Conn)
-	if !ok {
-		return nil
-	}
-	rawConn, err := sysConn.SyscallConn()
-	if err != nil {
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(time.Second))
+	defer cancel()
+	if err := c.ping(ctx); err != nil {
 		return err
 	}
-
-	err = rawConn.Read(func(fd uintptr) bool {
-		var buf [1]byte
-		n, err := syscall.Read(int(fd), buf[:])
-		switch {
-		case n == 0 && err == nil:
-			sysErr = io.EOF
-		case n > 0:
-			sysErr = errors.New("unexpected read from socket")
-		case err == syscall.EAGAIN || err == syscall.EWOULDBLOCK:
-			sysErr = nil
-		default:
-			sysErr = err
-		}
-		return true
-	})
-	if err != nil {
-		return err
-	}
-
-	return sysErr
+	return nil
 }
