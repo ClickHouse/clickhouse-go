2d66d30023e67033fc1368fa4035da2ee94c9a58
Kuba Kaflik <kuba.kaflik@clickhouse.com>
2023-08-02 14:42:52 +0200

    v2.12.1

--- client_info.go
+++ client_info.go
@@ -30,7 +30,7 @@ const ClientName = "clickhouse-go"
 const (
 	ClientVersionMajor       = 2
 	ClientVersionMinor       = 12
-	ClientVersionPatch       = 0
+	ClientVersionPatch       = 1
 	ClientTCPProtocolVersion = proto.DBMS_TCP_PROTOCOL_VERSION
 )
 
