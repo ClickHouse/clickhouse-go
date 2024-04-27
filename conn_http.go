18c54b07aa182b73d35342de656418cb90e9dd6b
Ivan Zhukov <electrocartridge@gmail.com>
2024-04-27 04:41:09 +0300

    Dont decompress a non-200 response

--- conn_http.go
+++ conn_http.go
@@ -424,10 +424,7 @@ func (h *httpConnect) readRawResponse(response *http.Response) (body []byte, err
 	if err != nil {
 		return nil, err
 	}
-	if response.StatusCode == http.StatusForbidden {
-		return nil, errors.New(string(body))
-	}
-	if h.compression == CompressionLZ4 || h.compression == CompressionZSTD {
+	if response.StatusCode == http.StatusOK && (h.compression == CompressionLZ4 || h.compression == CompressionZSTD) {
 		chReader := chproto.NewReader(reader)
 		chReader.EnableCompression()
 		reader = chReader
