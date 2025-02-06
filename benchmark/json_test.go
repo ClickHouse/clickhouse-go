cd14a4e5c8eaf6ed4ccbcc848bee851c72d62710
Spencer Torres <contact@spencertorres.com>
2025-02-06 02:23:33 -0500

    Add example/test/benchmark for JSON serializer, reorganize JSON helpers

--- benchmark/json_test.go
+++ benchmark/json_test.go
@@ -5,12 +5,10 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"github.com/ClickHouse/clickhouse-go/v2"
-	"github.com/ClickHouse/clickhouse-go/v2/lib/chcol"
 	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
 	clickhouse_tests "github.com/ClickHouse/clickhouse-go/v2/tests"
 	"os"
 	"testing"
-	"time"
 )
 
 const testSet string = "json_bench"
@@ -79,7 +77,7 @@ func prepareJSONReadTest(ctx context.Context, b *testing.B) (driver.Conn, driver
 		b.Fatal(err)
 	}
 
-	jsonRow := buildTestJSONPaths()
+	jsonRow := clickhouse_tests.BuildFastTestJSONStruct()
 	for i := 0; i < b.N; i++ {
 		if err := batch.Append(jsonRow); err != nil {
 			b.Fatal(err)
@@ -98,86 +96,6 @@ func prepareJSONReadTest(ctx context.Context, b *testing.B) (driver.Conn, driver
 	return conn, rows
 }
 
-var jsonTestDate, _ = time.Parse(time.RFC3339, "2024-12-13T02:09:30.123Z")
-
-type Address struct {
-	Street  string `chType:"String"`
-	City    string `chType:"String"`
-	Country string `chType:"String"`
-}
-
-type TestStruct struct {
-	Name   string
-	Age    int64
-	Active bool
-	Score  float64
-
-	Tags    []string
-	Numbers []int64
-
-	Address Address
-
-	KeysNumbers map[string]int64
-	Metadata    map[string]interface{}
-
-	Timestamp time.Time `chType:"DateTime64(3)"`
-
-	DynamicString chcol.Dynamic
-	DynamicInt    chcol.Dynamic
-	DynamicMap    chcol.Dynamic
-}
-
-func buildTestJSONPaths() *chcol.JSON {
-	jsonRow := chcol.NewJSON()
-	jsonRow.SetValueAtPath("Name", "JSON")
-	jsonRow.SetValueAtPath("Age", int64(42))
-	jsonRow.SetValueAtPath("Active", true)
-	jsonRow.SetValueAtPath("Score", 3.14)
-	jsonRow.SetValueAtPath("Tags", []string{"a", "b"})
-	jsonRow.SetValueAtPath("Numbers", []int64{20, 40})
-	jsonRow.SetValueAtPath("Address.Street", "Street")
-	jsonRow.SetValueAtPath("Address.City", "City")
-	jsonRow.SetValueAtPath("Address.Country", "Country")
-	jsonRow.SetValueAtPath("KeysNumbers", map[string]int64{"FieldA": 42, "FieldB": 32})
-	jsonRow.SetValueAtPath("Metadata.FieldA", "a")
-	jsonRow.SetValueAtPath("Metadata.FieldB", "b")
-	jsonRow.SetValueAtPath("Metadata.FieldC.FieldD", "d")
-	jsonRow.SetValueAtPath("Timestamp", jsonTestDate)
-	jsonRow.SetValueAtPath("DynamicString", clickhouse.NewDynamic("str"))
-	jsonRow.SetValueAtPath("DynamicInt", clickhouse.NewDynamic(int64(48)))
-	jsonRow.SetValueAtPath("DynamicMap", clickhouse.NewDynamic(map[string]string{"a": "a", "b": "b"}))
-
-	return jsonRow
-}
-
-func buildTestJSONStruct() TestStruct {
-	return TestStruct{
-		Name:    "JSON",
-		Age:     42,
-		Active:  true,
-		Score:   3.14,
-		Tags:    []string{"a", "b"},
-		Numbers: []int64{20, 40},
-		Address: Address{
-			Street:  "Street",
-			City:    "City",
-			Country: "Country",
-		},
-		KeysNumbers: map[string]int64{"FieldA": 42, "FieldB": 32},
-		Metadata: map[string]interface{}{
-			"FieldA": "a",
-			"FieldB": "b",
-			"FieldC": map[string]interface{}{
-				"FieldD": "d",
-			},
-		},
-		Timestamp:     jsonTestDate,
-		DynamicString: chcol.NewDynamic("str").WithType("String"),
-		DynamicInt:    chcol.NewDynamic(int64(48)).WithType("Int64"),
-		DynamicMap:    chcol.NewDynamic(map[string]string{"a": "a", "b": "b"}).WithType("Map(String, String)"),
-	}
-}
-
 // BenchmarkJSONInsert tests the performance for appending to a JSON column batch
 func BenchmarkJSONInsert(b *testing.B) {
 	b.Run("paths", func(b *testing.B) {
@@ -185,7 +103,7 @@ func BenchmarkJSONInsert(b *testing.B) {
 		conn, batch := prepareJSONInsertTest(ctx, b)
 		defer conn.Close()
 
-		jsonRow := buildTestJSONPaths()
+		jsonRow := clickhouse_tests.BuildTestJSONPaths()
 
 		b.ReportAllocs()
 		b.ResetTimer()
@@ -202,31 +120,7 @@ func BenchmarkJSONInsert(b *testing.B) {
 		conn, batch := prepareJSONInsertTest(ctx, b)
 		defer conn.Close()
 
-		inputRow := TestStruct{
-			Name:    "JSON",
-			Age:     42,
-			Active:  true,
-			Score:   3.14,
-			Tags:    []string{"a", "b"},
-			Numbers: []int64{20, 40},
-			Address: Address{
-				Street:  "Street",
-				City:    "City",
-				Country: "Country",
-			},
-			KeysNumbers: map[string]int64{"FieldA": 42, "FieldB": 32},
-			Metadata: map[string]interface{}{
-				"FieldA": "a",
-				"FieldB": "b",
-				"FieldC": map[string]interface{}{
-					"FieldD": "d",
-				},
-			},
-			Timestamp:     jsonTestDate,
-			DynamicString: chcol.NewDynamic("str").WithType("String"),
-			DynamicInt:    chcol.NewDynamic(int64(48)).WithType("Int64"),
-			DynamicMap:    chcol.NewDynamic(map[string]string{"a": "a", "b": "b"}).WithType("Map(String, String)"),
-		}
+		inputRow := clickhouse_tests.BuildTestJSONStruct()
 
 		b.ReportAllocs()
 		b.ResetTimer()
@@ -238,12 +132,29 @@ func BenchmarkJSONInsert(b *testing.B) {
 		b.StopTimer()
 	})
 
+	b.Run("fast_structs", func(b *testing.B) {
+		ctx := context.Background()
+		conn, batch := prepareJSONInsertTest(ctx, b)
+		defer conn.Close()
+
+		inputRow := clickhouse_tests.BuildFastTestJSONStruct()
+
+		b.ReportAllocs()
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			if err := batch.Append(&inputRow); err != nil {
+				b.Fatal(err)
+			}
+		}
+		b.StopTimer()
+	})
+
 	b.Run("marshal_strings", func(b *testing.B) {
 		ctx := context.Background()
 		conn, batch := prepareJSONInsertTest(ctx, b)
 		defer conn.Close()
 
-		inputRow := buildTestJSONStruct()
+		inputRow := clickhouse_tests.BuildTestJSONStruct()
 
 		b.ReportAllocs()
 		b.ResetTimer()
@@ -265,7 +176,7 @@ func BenchmarkJSONInsert(b *testing.B) {
 		conn, batch := prepareJSONInsertTest(ctx, b)
 		defer conn.Close()
 
-		inputRow := buildTestJSONStruct()
+		inputRow := clickhouse_tests.BuildTestJSONStruct()
 
 		inputRowStr, err := json.Marshal(inputRow)
 		if err != nil {
@@ -314,7 +225,26 @@ func BenchmarkJSONRead(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			rows.Next()
 
-			var row TestStruct
+			var row clickhouse_tests.TestStruct
+			err := rows.Scan(&row)
+			if err != nil {
+				b.Fatal(err)
+			}
+		}
+		b.StopTimer()
+	})
+
+	b.Run("fast_structs", func(b *testing.B) {
+		ctx := context.Background()
+		conn, rows := prepareJSONReadTest(ctx, b)
+		defer conn.Close()
+
+		b.ReportAllocs()
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			rows.Next()
+
+			var row clickhouse_tests.FastTestStruct
 			err := rows.Scan(&row)
 			if err != nil {
 				b.Fatal(err)
@@ -334,21 +264,8 @@ func BenchmarkJSONRead(b *testing.B) {
 
 // BenchmarkJSONMarshal compares the different ways to turn JSON data back into a string
 func BenchmarkJSONMarshal(b *testing.B) {
-	b.Run("paths_direct", func(b *testing.B) {
-		pathsRow := buildTestJSONPaths()
-
-		b.ReportAllocs()
-		b.ResetTimer()
-		for i := 0; i < b.N; i++ {
-			_, err := pathsRow.MarshalJSON()
-			if err != nil {
-				b.Fatal(err)
-			}
-		}
-	})
-
 	b.Run("paths", func(b *testing.B) {
-		pathsRow := buildTestJSONPaths()
+		pathsRow := clickhouse_tests.BuildTestJSONPaths()
 
 		b.ReportAllocs()
 		b.ResetTimer()
@@ -361,7 +278,7 @@ func BenchmarkJSONMarshal(b *testing.B) {
 	})
 
 	b.Run("structs", func(b *testing.B) {
-		structRow := buildTestJSONStruct()
+		structRow := clickhouse_tests.BuildTestJSONStruct()
 
 		b.ReportAllocs()
 		b.ResetTimer()
