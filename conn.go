1d90cb660fa9f78b7b2abfd1258903dadc2ba359
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2025-02-11 14:48:27 -0300

    perf(compressor): do not allocate a compressor if not configured

--- conn.go
+++ conn.go
@@ -74,10 +74,12 @@ func dial(ctx context.Context, addr string, num int, opt *Options) (*connect, er
 	}
 
 	compression := CompressionNone
+	var compressor *compress.Writer
 	if opt.Compression != nil {
 		switch opt.Compression.Method {
 		case CompressionLZ4, CompressionZSTD, CompressionNone:
 			compression = opt.Compression.Method
+			compressor = compress.NewWriter()
 		default:
 			return nil, fmt.Errorf("unsupported compression method for native protocol")
 		}
@@ -95,7 +97,7 @@ func dial(ctx context.Context, addr string, num int, opt *Options) (*connect, er
 			structMap:            &structMap{},
 			compression:          compression,
 			connectedAt:          time.Now(),
-			compressor:           compress.NewWriter(),
+			compressor:           compressor,
 			readTimeout:          opt.ReadTimeout,
 			blockBufferSize:      opt.BlockBufferSize,
 			maxCompressionBuffer: opt.MaxCompressionBuffer,
@@ -215,6 +217,7 @@ func (c *connect) close() error {
 	}
 
 	c.buffer = nil
+	c.compressor = nil
 
 	c.readerMutex.Lock()
 	c.reader = nil
