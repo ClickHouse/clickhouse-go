8374597363112c432be824fc2dd1d7cbf684ef96
Spencer Torres <contact@spencertorres.com>
2025-02-16 12:50:37 -0500

    include Variant and Dynamic array support

--- lib/column/variant.go
+++ lib/column/variant.go
@@ -275,8 +275,13 @@ func (c *Variant) encodeData(buffer *proto.Buffer) {
 	}
 }
 
-func (c *Variant) Encode(buffer *proto.Buffer) {
+func (c *Variant) WriteStatePrefix(buffer *proto.Buffer) error {
 	c.encodeHeader(buffer)
+
+	return nil
+}
+
+func (c *Variant) Encode(buffer *proto.Buffer) {
 	c.encodeData(buffer)
 }
 
@@ -336,13 +341,17 @@ func (c *Variant) decodeData(reader *proto.Reader, rows int) error {
 	return nil
 }
 
-func (c *Variant) Decode(reader *proto.Reader, rows int) error {
+func (c *Variant) ReadStatePrefix(reader *proto.Reader) error {
 	err := c.decodeHeader(reader)
 	if err != nil {
 		return fmt.Errorf("failed to decode variant header: %w", err)
 	}
 
-	err = c.decodeData(reader, rows)
+	return nil
+}
+
+func (c *Variant) Decode(reader *proto.Reader, rows int) error {
+	err := c.decodeData(reader, rows)
 	if err != nil {
 		return fmt.Errorf("failed to decode variant data: %w", err)
 	}
