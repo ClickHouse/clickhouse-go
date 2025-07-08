f994fff546adf4a3f9145237e87483f99e278883
Spencer Torres <contact@spencertorres.com>
2025-07-07 22:27:30 -0400

    Upgrade Dynamic type to serialization version 3

--- lib/column/column_gen.go
+++ lib/column/column_gen.go
@@ -138,8 +138,6 @@ func (t Type) Column(name string, tz *time.Location) (Interface, error) {
 		return &Point{name: name}, nil
 	case "String":
 		return &String{name: name, col: colStrProvider(name)}, nil
-	case "SharedVariant":
-		return &SharedVariant{name: name}, nil
 	case "Object('json')":
 		return &JSONObject{name: name, root: true, tz: tz}, nil
 	}
--- tests/dynamic_test.go
+++ tests/dynamic_test.go
@@ -19,7 +19,6 @@ package tests
 
 import (
 	"context"
-	"fmt"
 	"testing"
 	"time"
 
@@ -33,19 +32,16 @@ var dynamicTestDate, _ = time.Parse(time.RFC3339, "2024-12-13T02:09:30.123Z")
 func setupDynamicTest(t *testing.T, protocol clickhouse.Protocol) driver.Conn {
 	SkipOnCloud(t, "cannot modify Dynamic settings on cloud")
 
-	if protocol == clickhouse.HTTP {
-		t.Skip("unsupported serialization version 2 on HTTP")
-	}
-
 	conn, err := GetNativeConnection(t, protocol, clickhouse.Settings{
 		"max_execution_time":              60,
 		"allow_experimental_dynamic_type": true,
+		"output_format_native_use_flattened_dynamic_and_json_serialization": 1,
 	}, nil, &clickhouse.Compression{
 		Method: clickhouse.CompressionLZ4,
 	})
 	require.NoError(t, err)
 
-	if !CheckMinServerServerVersion(conn, 24, 8, 0) {
+	if !CheckMinServerServerVersion(conn, 25, 6, 0) {
 		t.Skip("unsupported clickhouse version for Dynamic type")
 	}
 
@@ -143,22 +139,85 @@ func TestDynamic(t *testing.T) {
 	})
 }
 
+func TestDynamicMaxTypes(t *testing.T) {
+	TestProtocols(t, func(t *testing.T, protocol clickhouse.Protocol) {
+		conn := setupDynamicTest(t, protocol)
+		ctx := context.Background()
+
+		const ddl = `
+			CREATE TABLE IF NOT EXISTS test_dynamic_max_types (
+				  c Dynamic(max_types=2)              
+			) Engine = MergeTree() ORDER BY tuple()
+		`
+		require.NoError(t, conn.Exec(ctx, ddl))
+		defer func() {
+			require.NoError(t, conn.Exec(ctx, "DROP TABLE IF EXISTS test_dynamic_max_types"))
+		}()
+
+		batch, err := conn.PrepareBatch(ctx, "INSERT INTO test_dynamic_max_types (c)")
+		require.NoError(t, err)
+
+		// Max types is set to 2, we want to try to fit 3 types + Null row into the Dynamic.
+
+		// Append String first to confirm types don't need to be sorted before sending to server.
+		colString := "test"
+		require.NoError(t, batch.Append(clickhouse.NewDynamicWithType(colString, "String")))
+		colInt64 := int64(42)
+		require.NoError(t, batch.Append(clickhouse.NewDynamicWithType(colInt64, "Int64")))
+		// The Null discriminator index will be equal to the total number of types.
+		var colNil any = nil
+		require.NoError(t, batch.Append(colNil))
+		// Append a new type to confirm that the Null discriminator is updated to match the new total types.
+		require.NoError(t, batch.Append(clickhouse.NewDynamicWithType(true, "Bool")))
+
+		require.NoError(t, batch.Send())
+
+		rows, err := conn.Query(ctx, "SELECT c FROM test_dynamic_max_types")
+		require.NoError(t, err)
+
+		var row clickhouse.Dynamic
+
+		require.True(t, rows.Next())
+		err = rows.Scan(&row)
+		require.NoError(t, err)
+		require.Equal(t, colString, row.Any())
+
+		require.True(t, rows.Next())
+		err = rows.Scan(&row)
+		require.NoError(t, err)
+		require.Equal(t, colInt64, row.Any())
+
+		require.True(t, rows.Next())
+		err = rows.Scan(&row)
+		require.NoError(t, err)
+		require.Equal(t, colNil, row.Any())
+
+		require.True(t, rows.Next())
+		err = rows.Scan(&row)
+		require.NoError(t, err)
+		require.Equal(t, true, row.Any())
+
+		require.NoError(t, rows.Close())
+		require.NoError(t, rows.Err())
+	})
+}
+
 func TestDynamicArray(t *testing.T) {
 	TestProtocols(t, func(t *testing.T, protocol clickhouse.Protocol) {
 		conn := setupDynamicTest(t, protocol)
 		ctx := context.Background()
 
 		const ddl = `
-			CREATE TABLE IF NOT EXISTS test_dynamic (
+			CREATE TABLE IF NOT EXISTS test_dynamic_array (
 				  c Array(Dynamic)                  
 			) Engine = MergeTree() ORDER BY tuple()
 		`
 		require.NoError(t, conn.Exec(ctx, ddl))
 		defer func() {
-			require.NoError(t, conn.Exec(ctx, "DROP TABLE IF EXISTS test_dynamic"))
+			require.NoError(t, conn.Exec(ctx, "DROP TABLE IF EXISTS test_dynamic_array"))
 		}()
 
-		batch, err := conn.PrepareBatch(ctx, "INSERT INTO test_dynamic (c)")
+		batch, err := conn.PrepareBatch(ctx, "INSERT INTO test_dynamic_array (c)")
 		require.NoError(t, err)
 
 		batch.Append([]clickhouse.Dynamic{
@@ -167,7 +226,7 @@ func TestDynamicArray(t *testing.T) {
 		})
 		require.NoError(t, batch.Send())
 
-		rows, err := conn.Query(ctx, "SELECT c FROM test_dynamic")
+		rows, err := conn.Query(ctx, "SELECT c FROM test_dynamic_array")
 		require.NoError(t, err)
 
 		var arrRow []clickhouse.Dynamic
@@ -191,22 +250,22 @@ func TestDynamicEmptyArray(t *testing.T) {
 		ctx := context.Background()
 
 		const ddl = `
-			CREATE TABLE IF NOT EXISTS test_dynamic (
+			CREATE TABLE IF NOT EXISTS test_dynamic_empty_array (
 				  c Array(Dynamic)                  
 			) Engine = MergeTree() ORDER BY tuple()
 		`
 		require.NoError(t, conn.Exec(ctx, ddl))
 		defer func() {
-			require.NoError(t, conn.Exec(ctx, "DROP TABLE IF EXISTS test_dynamic"))
+			require.NoError(t, conn.Exec(ctx, "DROP TABLE IF EXISTS test_dynamic_empty_array"))
 		}()
 
-		batch, err := conn.PrepareBatch(ctx, "INSERT INTO test_dynamic (c)")
+		batch, err := conn.PrepareBatch(ctx, "INSERT INTO test_dynamic_empty_array (c)")
 		require.NoError(t, err)
 
 		batch.Append([]clickhouse.Dynamic{})
 		require.NoError(t, batch.Send())
 
-		rows, err := conn.Query(ctx, "SELECT c FROM test_dynamic")
+		rows, err := conn.Query(ctx, "SELECT c FROM test_dynamic_empty_array")
 		require.NoError(t, err)
 
 		var arrRow []clickhouse.Dynamic
@@ -227,16 +286,16 @@ func TestDynamic_ScanWithType(t *testing.T) {
 		ctx := context.Background()
 
 		const ddl = `
-			CREATE TABLE IF NOT EXISTS test_dynamic (
+			CREATE TABLE IF NOT EXISTS test_dynamic_scan_with_type (
 				  c Dynamic                 
 			) Engine = MergeTree() ORDER BY tuple()
 		`
 		require.NoError(t, conn.Exec(ctx, ddl))
 		defer func() {
-			require.NoError(t, conn.Exec(ctx, "DROP TABLE IF EXISTS test_dynamic"))
+			require.NoError(t, conn.Exec(ctx, "DROP TABLE IF EXISTS test_dynamic_scan_with_type"))
 		}()
 
-		batch, err := conn.PrepareBatch(ctx, "INSERT INTO test_dynamic (c)")
+		batch, err := conn.PrepareBatch(ctx, "INSERT INTO test_dynamic_scan_with_type (c)")
 		require.NoError(t, err)
 
 		require.NoError(t, batch.Append(clickhouse.NewDynamicWithType(true, "Bool")))
@@ -244,7 +303,7 @@ func TestDynamic_ScanWithType(t *testing.T) {
 		require.NoError(t, batch.Append(nil))
 		require.NoError(t, batch.Send())
 
-		rows, err := conn.Query(ctx, "SELECT c FROM test_dynamic")
+		rows, err := conn.Query(ctx, "SELECT c FROM test_dynamic_scan_with_type")
 		require.NoError(t, err)
 
 		var row clickhouse.Dynamic
@@ -273,23 +332,23 @@ func TestDynamic_ScanWithType(t *testing.T) {
 }
 
 func TestDynamic_BatchFlush(t *testing.T) {
-	t.Skip(fmt.Errorf("server-side Dynamic bug"))
-
 	TestProtocols(t, func(t *testing.T, protocol clickhouse.Protocol) {
+		SkipOnHTTP(t, protocol, "Flush")
+
 		conn := setupDynamicTest(t, protocol)
 		ctx := context.Background()
 
 		const ddl = `
-			CREATE TABLE IF NOT EXISTS test_dynamic (
+			CREATE TABLE IF NOT EXISTS test_dynamic_batch_flush (
 				  c Dynamic                 
 			) Engine = MergeTree() ORDER BY tuple()
 		`
 		require.NoError(t, conn.Exec(ctx, ddl))
 		defer func() {
-			require.NoError(t, conn.Exec(ctx, "DROP TABLE IF EXISTS test_dynamic"))
+			require.NoError(t, conn.Exec(ctx, "DROP TABLE IF EXISTS test_dynamic_batch_flush"))
 		}()
 
-		batch, err := conn.PrepareBatch(ctx, "INSERT INTO test_dynamic (c)")
+		batch, err := conn.PrepareBatch(ctx, "INSERT INTO test_dynamic_batch_flush (c)")
 		require.NoError(t, err)
 
 		vals := make([]clickhouse.Dynamic, 0, 1000)
@@ -305,7 +364,7 @@ func TestDynamic_BatchFlush(t *testing.T) {
 		}
 		require.NoError(t, batch.Send())
 
-		rows, err := conn.Query(ctx, "SELECT c FROM test_dynamic")
+		rows, err := conn.Query(ctx, "SELECT c FROM test_dynamic_batch_flush")
 		require.NoError(t, err)
 
 		i := 0
