ee01ff0376d91fc0f3e35ec4e9b84168c351254c
Kaviraj <kavirajkanagaraj@gmail.com>
2025-10-16 13:46:17 +0200

    bug: Fixes some correctness bug with ContextWatchdog
    1. We don't need infinite for loop to call the callback once context
       timeout
    2. Currently callback will be called multiple times even after context
       is cancelled. This PR fixes and block the behavior with tests to make
       sure callback is called only once.
    3. Make sure the goroutine started by watchdog exits at all cases
       without any leaking
    
    Signed-off-by: Kaviraj <kavirajkanagaraj@gmail.com>

---
+++ context_watchdog_test.go
@@ -0,0 +1,89 @@
+package clickhouse
+
+import (
+	"context"
+	"sync/atomic"
+	"testing"
+	"testing/synctest"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestContextWatchdog(t *testing.T) {
+	t.Run("callback should be called once", func(t *testing.T) {
+		called := atomic.Int32{}
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+		defer cancel()
+
+		stopCW := contextWatchdog(ctx, func() {
+			called.Add(1)
+		})
+
+		<-ctx.Done()
+
+		// Give it some more time to make sure watch dog has enough time to
+		// call callback multiple times
+		time.Sleep(100 * time.Millisecond)
+		assert.Equal(t, int32(1), called.Load(), "callback should be called only once")
+
+		stopCW()
+		assert.Equal(t, int32(1), called.Load(), "callback should be called only once even after stopping watchdog")
+	})
+
+	t.Run("callback should not be called during normal exit before context cancellation", func(t *testing.T) {
+		called := atomic.Int32{}
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		stopCW := contextWatchdog(ctx, func() {
+			called.Add(1)
+		})
+		stopCW() // normal exit
+
+		assert.Equal(t, int32(0), called.Load(), "callback should not be called during normal exit")
+	})
+
+	t.Run("No goroutines should be left out after stopping ContextWatchdog", func(t *testing.T) {
+		synctest.Test(t, func(t *testing.T) {
+			// when context is cancelled
+			called := atomic.Int32{}
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+			defer cancel()
+
+			stopCW := contextWatchdog(ctx, func() {
+				called.Add(1)
+			})
+
+			<-ctx.Done()
+
+			// Give it some more time to make sure watch dog has enough time to
+			// call callback multiple times
+			time.Sleep(100 * time.Millisecond)
+			assert.Equal(t, int32(1), called.Load(), "callback should be called only once")
+
+			stopCW()
+			assert.Equal(t, int32(1), called.Load(), "callback should be called only once even after stopping watchdog")
+
+			synctest.Wait()
+		})
+
+		synctest.Test(t, func(t *testing.T) {
+			// during normal exit
+			called := atomic.Int32{}
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			stopCW := contextWatchdog(ctx, func() {
+				called.Add(1)
+			})
+			stopCW() // normal exit
+
+			assert.Equal(t, int32(0), called.Load(), "callback should not be called during normal exit")
+
+			synctest.Wait()
+		})
+	})
+
+	// No goroutines should be left out after stopping context watch dog
+}
