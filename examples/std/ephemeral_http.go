2a34102d7de922df6ab073eb399a7d508e15d128
Kaviraj <kavirajkanagaraj@gmail.com>
2025-11-12 13:36:16 +0100

    Add some examples for ephemeral column
    Covers
    1. Examples with both `std` and `clickhouse_api` interface
    2. Examples support both `Native` and `HTTP` protocol
    
    Signed-off-by: Kaviraj <kavirajkanagaraj@gmail.com>

---
+++ examples/std/ephemeral_http.go
@@ -0,0 +1,61 @@
+package std
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ClickHouse/clickhouse-go/v2"
+)
+
+func EphemeralColumnHTTP() error {
+	conn, err := GetStdOpenDBConnection(clickhouse.HTTP, nil, nil, nil)
+	if err != nil {
+		return fmt.Errorf("Failed to connect: %v\n", err)
+	}
+	defer conn.Close()
+
+	ctx := context.Background()
+	ddl := `CREATE OR REPLACE TABLE test
+(
+    id UInt64,
+    unhexed String EPHEMERAL,
+    hexed FixedString(4) DEFAULT unhex(unhexed)
+)
+ENGINE = MergeTree
+ORDER BY id;`
+	_, err = conn.ExecContext(ctx, ddl)
+	if err != nil {
+		return err
+	}
+
+	i := `INSERT INTO test (id, unhexed) VALUES (1, '5a90b714');`
+
+	_, err = conn.ExecContext(ctx, i)
+	if err != nil {
+		return err
+	}
+
+	query := `SELECT
+    id,
+    hexed,
+    hex(hexed)
+FROM test;`
+
+	rows, err := conn.QueryContext(ctx, query)
+	if err != nil {
+		return err
+	}
+
+	for rows.Next() {
+		var (
+			id  uint64
+			un  string
+			hex string
+		)
+		if err := rows.Scan(&id, &un, &hex); err != nil {
+			panic(err)
+		}
+		fmt.Println("id: ", id, "un: ", un, "hex: ", hex)
+	}
+	return nil
+}
